00d75d98cf115edaed8de9ff0afb06ec7fa8dd5a
charles <charles.englebert@protonmail.com>
2022-03-18 10:58:19 +0100

    accept null discount

--- src/product/models.py
+++ src/product/models.py
@@ -13,7 +13,7 @@ class Product(models.Model):
     price = models.FloatField()
     stock = models.IntegerField(default=0)
     department = models.CharField(max_length=128)
-    discount = models.ForeignKey(Discount, on_delete=models.CASCADE)
+    discount = models.ForeignKey(Discount, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
