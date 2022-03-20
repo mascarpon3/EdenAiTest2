5d4b77925dc845b358df975ebf89a8a600f823ef
charles <charles.englebert@protonmail.com>
2022-03-20 18:29:47 +0100

    refactor

--- src/cart/tests.py
+++ src/cart/tests.py
@@ -28,39 +28,36 @@ class CartAddProducts(APITestCase):
             department="electromenager",
             stock=2,
         )
+        cls.url = 'http://localhost:8000/api/cart/add_products'
         cls.user = User.objects.create(username="Eden")
         token = Token.objects.get(user__username='Eden')
         cls.headers = {'Authorization': 'Token ' + token.key}
         cls.api_client = APIClient(HTTP_AUTHORIZATION='Token ' + token.key)
 
     def test_add_one_product(self):
-        url = 'http://localhost:8000/api/cart/add_products'
         data = {"product_id": 1, "quantity": 1}
-        self.api_client.post(url, headers=self.headers, data=data)
+        self.api_client.post(self.url, headers=self.headers, data=data)
 
         cart_items = CartItems.objects.get(user=self.user, product=Product(id=data["product_id"]))
         self.assertEqual(cart_items.quantity, 1)
 
     def test_add_one_product_and_another(self):
-        url = 'http://localhost:8000/api/cart/add_products'
         data = {"product_id": 1, "quantity": 1}
-        self.api_client.post(url, headers=self.headers, data=data)
-        self.api_client.post(url, headers=self.headers, data=data)
+        self.api_client.post(self.url, headers=self.headers, data=data)
+        self.api_client.post(self.url, headers=self.headers, data=data)
 
         cart_items = CartItems.objects.get(user=self.user, product=Product(id=data["product_id"]))
         self.assertEqual(cart_items.quantity, 2)
 
     def test_add_Three_product(self):
-        url = 'http://localhost:8000/api/cart/add_products'
         data = {"product_id": 1, "quantity": 3}
-        self.api_client.post(url, headers=self.headers, data=data)
+        self.api_client.post(self.url, headers=self.headers, data=data)
 
         cart_items = CartItems.objects.get(user=self.user, product=Product(id=data["product_id"]))
         self.assertEqual(cart_items.quantity, 3)
 
     def test_add_more_products_than_there_are(self):
-        url = 'http://localhost:8000/api/cart/add_products'
         data = {"product_id": 1, "quantity": 12}
-        response = self.api_client.post(url, headers=self.headers, data=data).content.decode("utf-8")
+        response = self.api_client.post(self.url, headers=self.headers, data=data).content.decode("utf-8")
         expected = '{"stock":"you are trying to buy 12 cafetière(s) but we have only 3 left."}'
         self.assertEqual(expected, response)
