5996fb7c462e52e60a9c5a3516e53be8804bb39f
charles <charles.englebert@protonmail.com>
2022-03-20 15:48:17 +0100

    handle purchassing same article

--- src/cart/serializers.py
+++ src/cart/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from cart.models import CartItems
-from product.models import Product
+from django.db.models import Q
 
 
 class CartItemsSerializer(serializers.ModelSerializer):
@@ -9,18 +9,30 @@ class CartItemsSerializer(serializers.ModelSerializer):
         fields = ('user', 'product', 'quantity')
 
     def save(self):
+        existing_cart_items = CartItems.objects.filter(
+            Q(product=self.validated_data["product"].id) & Q(user=self.validated_data['user'].id)
+        )
+        if existing_cart_items:
+            assert len(existing_cart_items) == 1
+            cart_items = CartItems.objects.get(
+                product=self.validated_data["product"].id, user=self.validated_data['user'].id
+            )
+            cart_items.quantity += self.validated_data['quantity']
+        else:
+            cart_items = CartItems(
+                user=self.validated_data['user'],
+                product=self.validated_data['product'],
+                quantity=self.validated_data['quantity'],
+            )
 
-        if self.validated_data['product'].stock < self.validated_data['quantity']:
+        if self.validated_data['product'].stock < cart_items.quantity:
             raise serializers.ValidationError({
-                "stock": f"you are strying to buy {self.validated_data['quantity']} "
+                "stock": f"you are trying to buy {cart_items.quantity} "
                 f"{self.validated_data['product'].name}(s) but we have only "
                 f"{self.validated_data['product'].stock} left."
             })
 
-        cart_items = CartItems(
-            self.validated_data['user'].id,
-            self.validated_data['product'].id,
-            self.validated_data['quantity']
-        )
-
         cart_items.save()
+        response = f"{cart_items.quantity} {self.validated_data['product']}(s) successfully added to the cart."
+
+        return response, cart_items.quantity
--- src/cart/views.py
+++ src/cart/views.py
@@ -15,18 +15,19 @@ class AddProductToTheCart(APIView):
     def post(self, request, format=None):
         key = request.META["HTTP_AUTHORIZATION"].replace("Token ", "")
         user_id = Token.objects.get(key=key).user.id
+
         serializer = CartItemsSerializer(data={
             "product": request.data["product_id"],
             "user": user_id,
-            "quantity": request.data["quantity"]
+            "quantity": int(request.data["quantity"]),
         })
+
         if serializer.is_valid():
-            serializer.save()
-            product = Product.objects.get(id=request.data["product_id"])
+            response, quantity = serializer.save()
             data = {
-                "response": f"{request.data['quantity']} {product}(s) successfully added to the cart.",
-                "product": product.name,
-                "quantity": request.data["quantity"]
+                "response": response,
+                "product": Product.objects.get(id=request.data["product_id"]).name,
+                "quantity": quantity
             }
         else:
             data = serializer.errors
