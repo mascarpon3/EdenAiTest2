79f5a726f2cf538c8aa477a30eb37daf87d837df
charles <charles.englebert@protonmail.com>
2022-03-20 18:42:18 +0100

    rfactor

--- src/account/tests.py
+++ src/account/tests.py
@@ -10,43 +10,38 @@ class SignUpTest(TestCase):
             username="Eden",
             email="eden.test@hello.wolrd",
         )
+        cls.url = "http://localhost:8000/api/account/signup"
 
     def test_password_dont_match(self):
-        result = self.client.post(
-            "http://localhost:8000/api/account/signup",
-            data={
-                "username": "Pascale",
-                "password": "123",
-                "password2": "124",
-            }
-        ).content
+        data = {
+            "username": "Pascale",
+            "password": "123",
+            "password2": "124",
+        }
+        result = self.client.post(self.url, data=data).content
         expected = b'{"password":"password must match."}'
 
         self.assertEqual(expected, result)
 
     def test_username_already_exist(self):
-        result = self.client.post(
-            "http://localhost:8000/api/account/signup",
-            data={
-                "username": "Eden",
-                "password": "123",
-                "password2": "123",
-            }
-        ).content
+        data = {
+            "username": "Eden",
+            "password": "123",
+            "password2": "123",
+        }
+        result = self.client.post(self.url, data=data).content
         expected = b'{"username":["A user with that username already exists."]}'
         self.assertEqual(expected, result)
 
     def test_new_user_successuflly_register(self):
+        data = {
+            "username": "Charles",
+            "password": "123",
+            "password2": "123",
+            "email": "charles.test@hello.world"
+        }
         result = ast.literal_eval(
-            self.client.post(
-                "http://localhost:8000/api/account/signup",
-                data={
-                    "username": "Charles",
-                    "password": "123",
-                    "password2": "123",
-                    "email": "charles.test@hello.world"
-                }
-            ).content.decode("utf-8")
+            self.client.post(self.url, data=data).content.decode("utf-8")
         )["response"]
         expected = "new user successfully registered."
         self.assertEqual(expected, result)
@@ -63,14 +58,10 @@ class LogInTest(TestCase):
             password='good_password',
             email="eden.test@hello.wolrd",
         )
+        cls.url = "http://localhost:8000/api/account/login"
 
     def test_wrong_password(self):
-        result = self.client.post(
-            "http://localhost:8000/api/account/login",
-            data={
-                "username": "Eden",
-                "password": 'wrong_password',
-            }
-        ).content
+        data = {"username": "Eden", "password": 'wrong_password'}
+        result = self.client.post(self.url, data=data).content
         expected = b'{"non_field_errors":["Unable to log in with provided credentials."]}'
         self.assertEqual(expected, result)
